abd6736f6e781b2040f2ec3b6cb850775f26fd9e
Cpapa97 <cpapa97@gmail.com>
2023-05-25 17:30:13 -0500

    temporarily use setup.py due to pip breaking when creating a wheel for pyproject + docker-compose notebook support

--- neuropixel_pipeline/__init__.py
+++ neuropixel_pipeline/__init__.py
@@ -1,3 +1,8 @@
 from .version import __version__
 
+from . import api
+from . import readers
+from . import schemata
+from . import utils
+
 # None of Datajoint longblobs, external-blobs, attach, filepath, and attribute adapters should be used.
--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
 
-from setuptools import setup
+here = path.abspath(path.dirname(__file__))
 
-# Configuration is specified in pyproject.toml, which is used here as well
-if __name__ == "__main__":
-    setup()
+# read in version number into __version__
+with open(path.join(here, "neuropixel_pipeline", "version.py")) as f:
+    exec(f.read())
+
+with open(path.join(here, "requirements.txt")) as f:
+    requirements = [line.split("#", 1)[0].rstrip() for line in f.readlines()]
+
+setup(
+    name="neuropixel-pipeline",
+    version=__version__,  # noqa: F821
+    description="Schemata and libraries for ingesting and clustering Neuropixel data.",
+    author="Christos Papadopoulos",
+    author_email="cpapadop@bcm.edu",
+    packages=find_packages(exclude=[]),
+    install_requires=requirements,
+)
