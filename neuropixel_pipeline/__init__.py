73baa41007b162cfb693ffb55f216a9071779044
Cpapa97 <cpapa97@gmail.com>
2023-06-08 13:56:46 -0500

    testing fix for circular import

--- neuropixel_pipeline/__init__.py
+++ neuropixel_pipeline/__init__.py
@@ -2,9 +2,9 @@
 
 __version__ = "0.0.2"
 
-from . import api
-from . import readers
-from . import schemata
-from . import utils
+# from . import api
+# from . import readers
+# from . import schemata
+# from . import utils
 
-__all__ = ["api", "readers", "schemata", "utils", "__version__"]
+# __all__ = ["api", "readers", "schemata", "utils", "__version__"]
