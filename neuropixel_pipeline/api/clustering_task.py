841618fddf226202e833fb9df23817111d658251
Cpapa97 <cpapa97@gmail.com>
2023-06-19 07:04:08 -0500

    update kilosort runner to latest kilosort-runner api

--- neuropixel_pipeline/api/clustering_task.py
+++ neuropixel_pipeline/api/clustering_task.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 from enum import Enum
+from pathlib import Path
 
 
 class ClusteringTaskMode(str, Enum):
@@ -10,15 +11,26 @@ class ClusteringTaskMode(str, Enum):
 
 class ClusteringTaskRunner(BaseModel):
     task_mode: ClusteringTaskMode
+    data_dir: Path = None
+    results_dir: Path = None
+    filename: Path = None
     clustering_params: Optional[dict] = None
 
     def trigger_clustering(self):
         # Locally or using an HTTP request to a REST server
         if self.task_mode is ClusteringTaskMode.TRIGGER:
             try:
-                from kilosort_runner.run import KilosortParams
+                from kilosort_runner.run import KilosortRunner, KilosortParams
 
-                KilosortParams.model_validate(self.clustering_params).run_kilosort()
+                params = KilosortParams.model_validate(self.clustering_params)
+                runner = KilosortRunner.model_validate(
+                    data_dir=self.data_dir,
+                    results_dir=self.results_dir,
+                    filename=self.filename,
+                    params=params,
+                )
+                runner.run_kilosort()
+            
             except Exception as e:
                 print(f"Caught exception when trying to trigger Kilosort:\n{e}")
         elif self.task_mode is ClusteringTaskMode.LOAD:
