caa0c0fd1a093bd116f21ca7139e1991e106541a
Cpapa97 <cpapa97@gmail.com>
2023-06-26 15:23:06 -0500

    fix dictionary assignment

--- neuropixel_pipeline/api/postclustering.py
+++ neuropixel_pipeline/api/postclustering.py
@@ -240,8 +240,6 @@ class QualityMetricsRunner(BaseModel):
         kilosort_output_dir = Path(kilosort_output_dir)
 
         args = self.model_dump(by_alias=True)
-        args["quality_metrics_params"] = {
-            "quality_metrics_output_file": kilosort_output_dir / QUALITY_METRICS_FILE
-        }
+        args["quality_metrics_params"]["quality_metrics_output_file"] = kilosort_output_dir / QUALITY_METRICS_FILE
         args["directories"] = {"kilosort_output_directory": kilosort_output_dir}
         return calculate_quality_metrics(args)
